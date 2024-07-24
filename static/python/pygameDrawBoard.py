21706415061ee770cda53da76da477ea3ec34ce3
Unvire <krzysztof.balcerzak6@gmail.com>
2024-07-24 15:26:47 +0200

    Adjusts the maximum size of scaled board

--- static/python/pygameDrawBoard.py
+++ static/python/pygameDrawBoard.py
@@ -7,7 +7,7 @@ import component as comp
 class DrawBoardEngine:
     MIN_SURFACE_DIMENSION = 100
     STEP_FACTOR = 0.05
-    MAX_SURFACE_DIMENSION = 9000
+    MAX_SURFACE_DIMENSION = 8800
     DELTA_ROTATION_ANGLE_DEG = 5
 
     def __init__(self, width:int, height:int):
